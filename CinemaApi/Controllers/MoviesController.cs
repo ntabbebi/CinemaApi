4d1a0a0653d434f99db6eb5180ac7fb1d1092985
Nejemeddine Tabbebi <ntabbebi@gmail.com>
2020-12-15 01:40:00 +0100

    Add project files.

---
+++ CinemaApi/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaApi.Data;
+using CinemaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CinemaApi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+
+        public MoviesController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _dbContext.Movies;
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
